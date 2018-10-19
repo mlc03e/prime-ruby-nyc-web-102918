62eea5032ff0fcb1660c7ff83d5016b2b4a96bc8
Meghan Campbell <mlc03e@gmail.com>
2018-10-19 21:53:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(number)
   if number < 2 
-   return false
+    false
   end
  (2..(number - 1 )).each do |n|
     if number % n == 0
-     false
+     return false
     else 
      true
     end
